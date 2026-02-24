c9afc6faecc5ad006443967229ab8949619609fc
Yegor <egorgolovnia@gmail.com>
2026-02-24 17:12:45 +0100

    fix: completely remove orphaned Vercel import to unlock deployment pipeline

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_ZHwuPOEu.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
