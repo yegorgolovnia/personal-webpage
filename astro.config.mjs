a42dd9eec2d8270fc0c6db19355e7637d9dbf581
Yegor <egorgolovnia@gmail.com>
2026-02-24 22:03:20 +0100

    Revert Astro Vercel adapter addition, it causes 404 on static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    },
-    speedInsights: {
-      enabled: true
-    }
-  })
+  output: 'static'
 });
